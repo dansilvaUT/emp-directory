63b3c2c18020da310e6fb1febc2b01c23bb04586
Daniel Silva <danielsilva@mail.weber.edu>
2020-04-05 20:48:47 -0600

    About component.

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = (props) => (
+  <div className="main-content">
+    <h2>{props.title}</h2>
+    <p>Super Banking was founded in 1959, by Dwight K. Shcrute. Although, originally a beet farmer,
+        Mr. Schrute knew there was better ways to go about making money. At first he thought paper
+        but then realized how long term that was going to decline. He decided to build a banking corporation.</p>
+      <p>With a little help from his neighbors, Mr. Schrute was able to build a 3 billion dollar banking company
+        known as <strong>Super Banking</strong> As described by <em>Time Magazine </em>as one of the most powerful
+        banking companies in the world.</p>
+  </div>
+);
+
+export default About;
